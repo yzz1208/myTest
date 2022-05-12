115832e5ff55d443beced8a214033d6f4ddcc3cb
yzz1208 <94884597+yzz1208@users.noreply.github.com>
2022-05-12 20:31:00 +0800

    Update Hello.java

--- src/main/java/com/yyz/Hello.java
+++ src/main/java/com/yyz/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
         System.out.println("hello,git4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("pull test");
     }
 }
