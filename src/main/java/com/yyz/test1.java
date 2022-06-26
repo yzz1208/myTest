538ed73c24e4df8315c09d948c9c53c1406aef76
yyz <484671276@qq.com>
2022-06-26 10:17:14 +0800

    数据类型测试

--- src/main/java/com/yyz/test1.java
+++ src/main/java/com/yyz/test1.java
@@ -1,12 +1,34 @@
 package com.yyz;
 
+
+import org.junit.jupiter.api.Test;
+
 /**
  * @author 48467
  * @version 1.0
- * @description: TODO
+ * @description: //TODO
  * @date 2022/6/26 9:34
  */
 public class test1 {
 
+    public static void main(String[] args) {
 
+        System.out.println("a");
+    }
+
+    @Test
+    public void test_1() {
+        System.out.println("hello world");
+        long a = 10L;
+        char b = 97;
+        char c= 'b'+1;
+        float d=1.0F;
+        double e =1.0;
+
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
 }
